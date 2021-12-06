673b2490c3725711c79c5d5fb9b0da31cd192c97
ytyz1307zzh <zhangzhihan719@qq.com>
2021-12-05 22:26:07 -0500

    fix a syntax error in shell script

--- scripts/graphtool_multithread.sh
+++ scripts/graphtool_multithread.sh
@@ -14,7 +14,7 @@ num_threads=(1 4 8 16)
 
 for data in "${graphs[@]}"; do
     for alg in "${algorithms[@]}"; do
-        for n in "${num_threads[@]}" do
+        for n in "${num_threads[@]}"; do
             python code/graphtool/graphtool_"$alg".py data/"$data".txt "$n"
             echo
         done
