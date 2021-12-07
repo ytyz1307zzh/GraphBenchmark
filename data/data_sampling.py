88278029ed2c2f76b86cf3cc2f22360c91b096b9
ytyz1307zzh <zhangzhihan719@qq.com>
2021-12-06 21:48:30 -0500

    data sampling script

---
+++ data/data_sampling.py
@@ -0,0 +1,38 @@
+"""
+Down-sample a sub-graph of the original graph.
+"""
+
+import argparse
+from tqdm import tqdm
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-input', required=True, help='path to input file')
+parser.add_argument('-max_node', required=True, type=int, help='The number of nodes to keep')
+parser.add_argument('-output', required=True, help='path to output file')
+opt = parser.parse_args()
+
+edge_cnt = 0
+node_set = set()
+
+with open(opt.intput, 'r', encoding='utf8') as fin:
+    with open(opt.output, 'w', encoding='utf8') as fout:
+
+        for line in tqdm(fin.readlines()):
+            edge = line.strip().split()
+            from_node = int(edge[0])
+            to_node = int(edge[1])
+
+            if from_node >= opt.max_node or to_node >= opt.max_node:
+                continue
+
+            fout.write(line)
+            edge_cnt += 1
+
+            if from_node not in node_set:
+                node_set.add(from_node)
+            if to_node not in node_set:
+                node_set.add(to_node)
+
+print(f'A sub-graph with {len(node_set)} nodes and {edge_cnt} edges created.')
+
